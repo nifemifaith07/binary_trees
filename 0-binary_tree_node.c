29df0bb64c3b10769ea8e6c32b726fbbf5152c5b
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-04 02:38:03 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,12 +10,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = NULL;
+	binary_tree_t *newnode;
 
 	newnode = malloc(sizeof(binary_tree_t));
 	if (!newnode)
 		return (NULL);
 	newnode->n = value;
 	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
 	return (newnode);
 }
