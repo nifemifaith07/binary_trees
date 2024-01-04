5c311a6079f2433718e40070049d4e0b9b29d152
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-04 21:43:16 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	size_t lt = 0, size_t rt = 0;
