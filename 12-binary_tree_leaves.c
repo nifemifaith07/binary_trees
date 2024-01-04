2f3c2894ff0db5ff4bc7a0979973e99b4158335d
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-04 22:52:55 +0100

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: ptr to the root node of the tree to count the number of leaves
+ *
+ * Return: 0, If tree is NULL, otherwise, no of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaf = 0;
+
+	if (tree)
+	{
+		leaf += (!tree->left && !tree->right) ? 1 : 0;
+		leaf += binary_tree_leaves(tree->left);
+		leaf += binary_tree_leaves(tree->right);
+	}
+	return (leaf);	
+}
