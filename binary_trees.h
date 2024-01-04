6f01a1af71c5f27e92cd77fc88e3e852d4db2b37
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-04 01:38:54 +0100

    Create binary_trees.h

---
+++ binary_trees.h
@@ -0,0 +1,42 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+/* Basic binary tree */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary search tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL tree */
+typedef struct binary_tree_s avl_t;
+
+/* max binary heap */
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BINARY_TREES_H */
