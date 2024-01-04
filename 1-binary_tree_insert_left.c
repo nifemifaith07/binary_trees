6928abb6aa4fcd236fa436d428d98b4e5acd868d
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-04 03:36:25 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,6 +32,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		temp = parent->left;
 		parent->left = new_node;
+		temp->parent = new_node;
 		new_node->left = temp;
 	}
 	return (new_node);
