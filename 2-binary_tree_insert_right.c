a3078cd2e9c2824c74d38c7c267d0a35ea50402c
nifemifaith07 <nifemifaith07@gmail.com>
2024-01-03 18:53:09 -0800

    make executable

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as rightchild of another node
  * @parent: pointer to the node to insert the right-child in
  * @value: the value to store in the new node
  *
