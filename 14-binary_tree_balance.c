f50431593218de8dbca61fef5930159926b57c6f
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-05 05:16:11 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,10 +22,10 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t lt = 0;
+	rt = 0;
 	if (tree)
 	{
-		size_t lt = 0, rt = 0;
-
 		lt = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		rt = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((lt > rt) ? lt : rt);
